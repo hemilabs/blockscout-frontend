cd94f4af3d73ab9388563f6c01b2abc7d6f5f5cb
Gabriel Montes <gabriel@hemi.xyz>
2025-01-02 11:14:02 -0300

    Rename Docker image

--- tools/scripts/docker.preset.sh
+++ tools/scripts/docker.preset.sh
@@ -10,13 +10,13 @@ config_file="./configs/envs/.env.${preset_name}"
 secrets_file="./configs/envs/.env.secrets"
 
 if [ ! -f "$config_file" ]; then
-    echo "Error: File '$config_file' not found."
-    exit 1
+  echo "Error: File '$config_file' not found."
+  exit 1
 fi
 
 if [ ! -f "$secrets_file" ]; then
-    echo "Error: File '$secrets_file' not found."
-    exit 1
+  echo "Error: File '$secrets_file' not found."
+  exit 1
 fi
 
-docker run -p 3000:3000 --env-file $config_file --env-file $secrets_file blockscout-frontend:local | pino-pretty
+docker run -p 3000:3000 --env-file $config_file --env-file $secrets_file hemilabs/blockscout-frontend:local | pino-pretty
